6d91da7c9661b0ce2df0fa0ee90bd0efc7994161
Saad711T <75455552+Saad711T@users.noreply.github.com>
2025-05-09 17:59:59 +0300

    Add backend files

---
+++ offshore-platform/backend/user_service/serializers.py
@@ -0,0 +1,9 @@
+#Libraries
+from rest_framework import serializers
+from .models import CustomUser
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'role']
